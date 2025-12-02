0b74062c2b312119b5dcc65a7f019f3441af764a
thanhbv <thanhbv343@gamil.com>
2025-12-02 13:30:03 +0700

    up giao diện login_admin

--- views/auth/login_admin.php
+++ views/auth/login_admin.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <title>Đăng nhập Admin</title>
-
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body class="bg-gray-100">
@@ -38,8 +38,6 @@
             </form>
         </div>
     </div>
-
-    <script src="https://cdn.tailwindcss.com"></script>
 </body>
 
 </html>
